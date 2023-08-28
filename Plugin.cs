7bf27fe0c00e3988c82275b99fab98ba6fd2c6d7
EzeTxle <60270488+EzeTxle@users.noreply.github.com>
2023-08-28 18:13:10 -0300

    Mod
    hi :)

---
+++ Plugin.cs
@@ -0,0 +1,63 @@
+﻿using SML;
+using UnityEngine;
+using HarmonyLib;
+using Services;
+using Cinematics.Players;
+using Server.Shared.Cinematics.Data;
+using Home.Services;
+using Utils;
+
+namespace Main
+{
+    [Mod.SalemMod]
+    public class Main
+    {
+        public static void Start()
+            {
+            Debug.Log("Working?"); 
+            }
+        
+    }
+
+    [HarmonyPatch(typeof(DeputyKillCinematicPlayer), "Init")]
+    class DeputyKillCinematicPlayer_Init_Patch{
+        static public void Prefix(DeputyKillCinematicPlayer __instance, ICinematicData cinematic){
+            DeputyKillCinematicData deputyKillCinematicData = cinematic as DeputyKillCinematicData;
+			int deputyPosition = deputyKillCinematicData.deputyPosition;
+			int otherPosition = deputyKillCinematicData.otherPosition;
+            ChatUtils.AddMessage(ModSettings.GetString("Deputy Message").Replace("%deputy%", "[[@"+(deputyPosition + 1) + "]]").Replace("%deputyRole%",TypesToTypesUtils.GetMentionEncodedText("#deputy")).Replace("%target%", "[[@"+(otherPosition + 1)+"]]"), "", false); //there are multiple styles you can use to paint the text however you want, but i want to give users the freedom to choose their text so im leaving it blank.
+        }
+        
+        }
+
+    [HarmonyPatch(typeof(ProsecutionCinematicPlayer), "Init")]
+    class ProsecutionCinematicPlayer_Init_Patch{
+        public static void Prefix(ProsecutionCinematicPlayer __instance, ICinematicData cinematic){
+            ProsecutionCinematicData prosecutionCinematicData = cinematic as ProsecutionCinematicData;
+			int prosecutorPosition = prosecutionCinematicData.prosecutorPostion;
+			int otherPosition = prosecutionCinematicData.targetPosition;
+            ChatUtils.AddFeedbackMsg("<color=blue>[[@"+(prosecutorPosition + 1)+"]] prosecuted [[@"+(otherPosition + 1)+"]]!</color>",false,"info");
+            ChatUtils.AddFeedbackMsg(ModSettings.GetString("Prosecutor Message").Replace("%prosecutor%", "[[@"+(prosecutorPosition + 1) + "]]").Replace("%prosecutorRole%",TypesToTypesUtils.GetMentionEncodedText("#prosecutor")).Replace("%target%", "[[@"+(otherPosition + 1)+"]]"),false,"info");
+        }
+    }
+    [HarmonyPatch(typeof(ConjurerKillCinematicPlayer), "Init")]
+    class ConjurerKillCinematicPlayer_Init_Patch{
+        public static void Prefix(ConjurerKillCinematicPlayer __instance, ICinematicData cinematic){
+            ConjurerKillCinematicData conjurerKillCinematicData = cinematic as ConjurerKillCinematicData;
+			
+			int otherPosition = conjurerKillCinematicData.otherPosition;
+            string txt = "critical";
+            if(conjurerKillCinematicData.didKill){
+                txt = "warning";
+            }
+            ChatUtils.AddFeedbackMsg(ModSettings.GetString("Conjurer Message").Replace("%conjurerRole%",TypesToTypesUtils.GetMentionEncodedText("#conjurer")).Replace("%target%", "[[@"+(otherPosition + 1)+"]]"),false,txt);
+        }
+        static string DidKill(bool check){
+            if(check){
+                return ".";
+            }
+            return " but they survived. They must be a "+TypesToTypesUtils.GetMentionEncodedText("#neutral apocalypse")+"!";
+        }
+    }
+        
+}
---
+++ Utils/TypesToTypesUtils.cs
@@ -0,0 +1,50 @@
+using Server.Shared.State;
+using System.Reflection;
+using System.Collections.Generic;
+using Mentions;
+using Mentions.UI;
+using UnityEngine;
+namespace Utils{
+    static public class TypesToTypesUtils{
+        static public string GetMentionEncodedText(string humanText){
+            if(humanText[0] != '@' && humanText[0] != '#' && humanText[0] != ':'){
+                Debug.LogError("Error: " + humanText + " does not start with a valid character. Always start with #, : or @");
+                return "Error: " + humanText + " does not start with a valid character";
+            }
+            MentionPanel mentionPanel = (MentionPanel)GameObject.FindObjectOfType(typeof(MentionPanel));
+            if(mentionPanel == null)
+            {
+                return "Error! MentionPanel not found";
+            }
+           foreach (MentionInfo mentionInfo in (List<MentionInfo>)mentionPanel.mentionsProvider.GetType().GetField("MentionInfos", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(mentionPanel.mentionsProvider))
+			{
+				if(mentionInfo.humanText == humanText)
+				{
+                    return mentionInfo.encodedText;
+				}
+            }
+           return "Error: " + humanText + " not found";
+        }
+
+
+        static public ClientFeedbackType StringToFeedbackType(string str){
+            switch(str){
+                case "normal":
+                    return ClientFeedbackType.Normal;
+                case "info":
+                    return ClientFeedbackType.Info;
+                case "warning":
+                    return ClientFeedbackType.Warning;
+                case "critical":
+                    return ClientFeedbackType.Critical;
+                case "success":
+                    return ClientFeedbackType.Success;
+                default:
+                    Debug.LogError("Error: " + str + " is not a valid feedback type, defaulting to normal");
+                    return ClientFeedbackType.Normal;
+            }
+        }    
+    }
+
+
+}
